c56eafc00de0f7b7a4358ef8441667c509ba5fc5
Ryan Dobrynski <rdobrynski@catbird.com>
2016-09-23 15:15:38 -0600

    all tests passing

--- leap/leap.js
+++ leap/leap.js
@@ -1,12 +1,19 @@
-var Year = function() {};
+var Year = function(input) {
+	this.inputYear = input
+};
+
 Year.prototype.isLeap = function(input){
-
-	if (input === 2015) {
-		return false
+	if (!(this.inputYear % 4)){
+		if (!(this.inputYear % 400)){
+			return true
+		} else if (!(this.inputYear % 100)){
+			return false
+		} else {
+			return true
+		}
 	} else {
-		return true
+		return false
 	}
-	return false
-
 }
+
 module.exports = Year;
